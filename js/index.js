a568aac4155c75e0f4bbbc714e838135672d8d66
MiguelCuellarR <72205477+MiguelCuellarR@users.noreply.github.com>
2021-07-29 20:43:58 -0500

    Primeros Halos de Esperanza

--- js/index.js
+++ js/index.js
@@ -1,18 +1,123 @@
 function Busqueda() {
-
-    let palabrasClave =[
+    let palabrasClave = [
         "lista",
-        "materia"
+        "materia",
+        "saldo", "solicitud", "subsidio"
+
     ];
 
     let cadena = document.getElementById('search').value;
-    let tamaño = cadena.length;
-    for (let indice in palabrasClave) {
-        let nombre = palabrasClave[indice];
-        let str = nombre.substring(0, tamaño);
-        if (cadena.length <= nombre.length && cadena.length != 0 && nombre.length !=0) {
-            
+    let estadoActual = 'q0'
+
+    while (estadoActual != 'z') {
+        let pos = 0;
+
+        if (cadena[pos] == 's' && estadoActual == 'q0') {
+            estadoActual = 'r44';
+            pos++
+            //saldo
+            if (cadena[pos] == 'a' && estadoActual == 'r44') {
+                pos++
+                estadoActual = 'r45';
+                if (cadena[pos] == 'l' && estadoActual == 'r45') {
+                    pos++
+                    estadoActual = 'r46';
+                    if (cadena[pos] == 'd' && estadoActual == 'r46') {
+                        pos++
+                        estadoActual = 'r47';
+                        if (cadena[pos] == 'o' && estadoActual == 'r47') {
+                            estadoActual = 'z';
+                            palabrasClave.forEach(palabra => {
+                                if (palabra == cadena) {
+                                    console.log(`Consultar Saldo Cuenta`)
+                                }
+                            });
+                            break;
+                        }
+                    }
+                }
+            }
+
+            //solicitud
+            if (cadena[pos] == 'o' && estadoActual == 'r44') {
+                pos++
+                estadoActual = 'r49';
+                if (cadena[pos] == 'l' && estadoActual == 'r49') {
+                    pos++
+                    estadoActual = 'r50';
+                    if (cadena[pos] == 'i' && estadoActual == 'r50') {
+                        pos++
+                        estadoActual = 'r51';
+                        if (cadena[pos] == 'c' && estadoActual == 'r51') {
+                            pos++
+                            estadoActual = 'r52';
+                            if (cadena[pos] == 'i' && estadoActual == 'r52') {
+                                pos++
+                                estadoActual = 'r53';
+                                if (cadena[pos] == 't' && estadoActual == 'r53') {
+                                    pos++
+                                    estadoActual = 'r54';
+                                    if (cadena[pos] == 'u' && estadoActual == 'r54') {
+                                        pos++
+                                        estadoActual = 'r55';
+                                        if (cadena[pos] == 'd' && estadoActual == 'r55') {
+                                            estadoActual = 'z';
+                                            palabrasClave.forEach(palabra => {
+                                                if (palabra == cadena) {
+                                                    console.log(`Generar Tramite de Certificados,                                                                                   
+                                                                Consultar Estado de Tramites de Certificados,  
+                                                                Carnet, Diploma y Grado`)
+                                                }
+                                            });
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            //subsidio
+            if (cadena[pos] == 'u' && estadoActual == 'r44') {
+                pos++
+                estadoActual = 'r57';
+                if (cadena[pos] == 'b' && estadoActual == 'r57') {
+                    pos++
+                    estadoActual = 'r58';
+                    if (cadena[pos] == 's' && estadoActual == 'r58') {
+                        pos++
+                        estadoActual = 'r59';
+                        if (cadena[pos] == 'i' && estadoActual == 'r59') {
+                            pos++
+                            estadoActual = 'r60';
+                            if (cadena[pos] == 'd' && estadoActual == 'r60') {
+                                pos++
+                                estadoActual = 'r61';
+                                if (cadena[pos] == 'i' && estadoActual == 'r61') {
+                                    pos++
+                                    estadoActual = 'r62';
+                                    if (cadena[pos] == 'o' && estadoActual == 'r62') {
+                                        estadoActual = 'z';
+                                        palabrasClave.forEach(palabra => {
+                                            if (palabra == cadena) {
+                                                console.log(`Inscripción de Becas de Compesación y Subsidio de Alimentación,
+                                                              Consultar formulario de  Becas de Compesación y Subsidio de Alimentación`)
+                                            }
+                                        });
+                                        break;
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
         }
+        break;
     }
 
 }
